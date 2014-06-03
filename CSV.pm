91709e9453abb93f6f8c6c1a5f2309ab1b75a220
Michal Špaček <tupinek@gmail.com>
2014-06-03 09:12:59 +0200

    Simplify to new version of Graph::Reader::TGF.

--- CSV.pm
+++ CSV.pm
@@ -12,24 +12,6 @@ use Text::CSV;
 # Version.
 our $VERSION = 0.01;
 
-# Edge callback.
-sub _edge_callback {
-	my ($self, $graph, $id1, $id2, $edge_label) = @_;
-	$graph->set_edge_attribute($id1, $id2, 'label', $edge_label);
-	return;
-}
-
-# Init.
-sub _init {
-	my ($self, $param_hr) = @_;
-	$self->SUPER::_init();
-	$self->{'edge_callback'} = $param_hr->{'edge_callback'}
-		|| \&_edge_callback;
-	$self->{'vertex_callback'} = $param_hr->{'vertex_callback'}
-		|| \&_vertex_callback;
-	return;
-}
-
 # Vertex callback.
 sub _vertex_callback {
 	my ($self, $graph, $id, $vertex_label) = @_;
