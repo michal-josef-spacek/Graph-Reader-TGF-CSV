a1e6c5ab9a91a793c7aaef35a544a43f45829d62
Michal Špaček <tupinek@gmail.com>
2015-10-29 14:27:35 +0100

    Improve SEE ALSO section in doc.

--- CSV.pm
+++ CSV.pm
@@ -162,13 +162,17 @@ L<Text::CSV>.
 
 =head1 SEE ALSO
 
-L<Graph::Reader>,
-L<Graph::Reader::Dot>,
-L<Graph::Reader::HTK>,
-L<Graph::Reader::LoadClassHierarchy>,
-L<Graph::Reader::UnicodeTree>,
-L<Graph::Reader::TGF>,
-L<Graph::Reader::XML>.
+=over
+
+=item L<Graph::Reader>
+
+base class for Graph file format readers
+
+=item L<Task::Graph::Reader>
+
+Install the Graph::Reader modules.
+
+=back
 
 =head1 REPOSITORY
 
